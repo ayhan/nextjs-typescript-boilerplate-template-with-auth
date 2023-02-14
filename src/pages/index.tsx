d7ea190cda40afcf02f10e792574c6edb5154f1b
Ayhan Kırıkcı <ayhankirikci2264@gmail.com>
2023-02-14 12:46:20 +0300

    chore: tailwind test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
@@ -10,8 +10,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1 className='text-3xl'>Hello World !</h1>
+        <h1 className="text-3xl">Hello World !</h1>
       </main>
     </>
-  )
+  );
 }
